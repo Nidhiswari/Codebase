1879617c1295a02225120968c3c7e668809645fb
Nidhiswari <114008923+Nidhiswari@users.noreply.github.com>
2022-09-20 21:24:25 +0530

    basic java

---
+++ src/basic/demo.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class demo {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("the addition of "+a+" + "+b+" is  args[0)+ args[1]");
+		
+		
+	}
+
+}
